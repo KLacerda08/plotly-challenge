175162b3559b38ef14d8974f2b8e625b66be9edf
KLacerda08 <kaldmr7880@gmail.com>
2021-01-15 19:40:18 -0500

    code progress on bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,74 +1,83 @@
-
 // Define a function that will create metadata for given sample
-function buildMetadata(sample) {
+function buildMetadata(inputValue) {
     // Read the json data
     d3.json("samples.json").then(function(response) {
-        console.log(response.metadata);
+        // console.log(response.metadata);
         // Assign a variable to filter the data, to get the sample's metadata (value)
         // based on the sample id (key). convert the sample id field to an integer
         // get the metadata for the first sample using index [0]
         // Select the input element and get the raw HTML node
         var inputElement = d3.select("#selDataset");
-        // Get the value property of the input element
+        // // Get the value property of the input element
         var inputValue = inputElement.property("value");
-        
-        // var filteredData = response.metadata.filter(record => record.id === parseInt(record.id))[0];
-        var filteredData = response.metadata.filter(record => record.id === parseInt(inputValue))[0];
-        // identify the html tag and clear it from it's previous load
+        var sampleData = response.metadata.filter(record => record.id === parseInt(inputValue))[0];
+        // identify the html element and clear it from it's previous load
         var sampleMeta = d3.select("#sample-metadata");
         sampleMeta.html("");
-        //this works
-        console.log(filteredData);
-        // the filtered data returns an array of objects, but we want to get object out of array
-        // first, define a variable for the html element (here, an id) 
-        var sampleMeta = d3.select("#sample-metadata");
+        // Get object out of filteredData array, and 
         // add a new paragraph to the html element for each key value pair
-        Object.entries(filteredData).forEach(function([key, value]) {
+        // add the text of the key value pair to each new paragraph
+        Object.entries(sampleData).forEach(function([key, value]) {
             var p = sampleMeta.append("p");
-            //add the text of the key value pair to each new paragraph
             p.text(`${key}: ${value}`);
         });
     });
 }
 
-// Define a function that will create charts for given sample
-function buildCharts(sample) {
+// Define a function that will create charts for given patient sample
+function buildCharts1(inputValue) {
     // Read the json data
     d3.json("samples.json").then(function(response) {
-        //this works
-        console.log(response.samples);
-        // Select the input element and get the raw HTML node
+        // Select the input element and get the value to correspond to the sample ID
         var inputElement = d3.select("#selDataset");
-        // Get the value property of the input element
         var inputValue = inputElement.property("value");
-        // create the filtered data set 
-        // var filteredData = response.metadata.filter(record => record.id === parseInt(inputValue))[0];
-        // var filteredData = response.samples.filter(record => record.id == parseInt(record.id))[0];
-        // var filteredData = response.samples.filter(record => record.id)[0];
-        var filteredData = response.samples.filter(record => parseInt(record.id) == parseInt(sample));
-        console.log(sample);
-        console.log(response.samples), 
-        console.log(response.samples[0].id)
-        // console.log(filteredData);
+        // Assign variable to json response 
+        var sampleData = response.samples;
+        
+        // Get data for charts: sample values (OTU Values), OTU IDs, and OTU labels    
+        var sampleId = sampleData.map(function(record) {
+            return record.id
+            }).indexOf(inputValue);       
+        var otuValues = sampleData.map(function(record) {
+            return record.sample_values
+            });
+        var otuIds = sampleData.map(function(record) {
+            return record.otu_ids
+            });
+        var otuLabels = sampleData.map(function(record) {
+            return record.otu_labels
+            });
+        // Get top ten from each array for plotting; data is already sorted in descending order
+        var valuesTopTen = otuValues[sampleId].slice(0,10).reverse();
+        var idsTopTen = otuIds[sampleId].slice(0,10).reverse();
+        var labelsTopTen = otuLabels[sampleId].slice(0,10).reverse();
+
         // identify the html tag and clear it from it's previous load
-        // var barChart = d3.select("#bar");
-        // barChart.html("");
-        // add a new bar chart to the html element for the selected patient
-        // Object.entries(filteredData).forEach(function([key, value]) {
-        //     var p = sampleMeta.append("p");
-        //     //add the text of the key value pair to each new paragraph
-        //     p.text(`${key}: ${value}`);
-    
-        // var 
-        // var topTen = 
-    // Parse and filter the data to get the sample's OTU data
-    // Pay attention to what data is required for each chart
+        var barChart = d3.select("#bar");
+        barChart.html("");
 
-    // Create bar chart in correct location
+        // Create the Trace
+        var trace1 = {
+            x: valuesTopTen,
+            // y: otuIds,
+            type: "bar"
+        };
 
-    // Create bubble chart in correct location
-    });
+        // Create the data array for the plot
+        var data = [trace1];
 
+        // Define the plot layout
+        var layout = {
+        title: "Top 10 Belly Button Microbes",
+        xaxis: { title: "Number of Bacteria Present" },
+        yaxis: { title: "Type of Bacteria (OTU ID)" }
+        };
+
+        // Plot the chart to the tag with the id "bar"
+        Plotly.newPlot("bar", data, layout);
+
+
+});
 }
 
 // Define function that will run on page load
@@ -88,48 +97,20 @@ function init() {
         });
     });
 }
-// Parse and filter data to get sample names
 
-// Use first sample to build metadata and initial plots
-
-function optionChanged(Sample) {
-    buildMetadata(Sample);
+// function to change output based on dropdown selection
+function optionChanged(newSampleId) {
+    buildMetadata(newSampleId);
+    buildCharts1(newSampleId);
 }
-
-
-// function optionChanged(newSample) {
-    // // Read the json data
-    // d3.json("samples.json").then(function(response) {
-    //     // Select the input element and get the raw HTML node
-    //     var inputElement = d3.select("#selDataset");
-    //     // Get the value property of the input element
-    //     var inputValue = inputElement.property("value");
-    //     // create the filtered data set 
-    //     var filteredData = response.metadata.filter(record => record.id === parseInt(inputValue))[0];
-    //     // identify the html tag and clear it from it's previous load
-    //     var sampleMeta = d3.select("#sample-metadata");
-    //     sampleMeta.html("");
-    //     // add a new paragraph to the html element for each key value pair
-    //     Object.entries(filteredData).forEach(function([key, value]) {
-    //         var p = sampleMeta.append("p");
-    //         //add the text of the key value pair to each new paragraph
-    //         p.text(`${key}: ${value}`);
-    //     });
-
-        //update charts 
-
-    // });
-// }
 
     // Update metadata with newly selected sample
     // console.log(newSample)
     // Update charts with newly selected sample
 
-
-
 // Initialize dashboard on page load
 init();
 buildMetadata();
-buildCharts();
+// buildCharts();
+buildCharts1();
 // optionChanged(newSample);
-
